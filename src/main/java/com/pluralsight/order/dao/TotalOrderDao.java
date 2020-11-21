db29f29417f524538ff96e3d2523ff69b0e9b9a9
Evynglais <hehir45@googlemail.com>
2020-11-21 17:04:03 +0000

    Module 7
    Completed module 7

--- src/main/java/com/pluralsight/order/dao/TotalOrderDao.java
+++ src/main/java/com/pluralsight/order/dao/TotalOrderDao.java
@@ -30,9 +30,17 @@ public class TotalOrderDao {
     public BigDecimal getTotalAllPaidOrders(ParamsDto paramsDto) {
         BigDecimal result = null;
 
-        try (Connection con = null;
+        try (Connection con = database.getConnection();
              CallableStatement cs = createCallableStatement(con, paramsDto.getCustomerId())
         ) {
+
+            cs.execute();
+
+            try (ResultSet rs = cs.getResultSet()) {
+                if (rs != null && rs.next()) {
+                    result = rs.getBigDecimal(1);
+                }
+            }
 
         } catch (SQLException ex) {
             ExceptionHandler.handleException(ex);
@@ -49,7 +57,8 @@ public class TotalOrderDao {
      * @throws SQLException In case of an error
      */
     private CallableStatement createCallableStatement(Connection con, long customerId) throws SQLException {
-
-        return null;
+        CallableStatement callableStatement = con.prepareCall(query);
+        callableStatement.setLong(1, customerId);
+        return callableStatement;
     }
 }
